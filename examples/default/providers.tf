dhoppeIT/terraform-adguard-rewrite

chore: Initial commit

examples/default/providers.tf
@@ -1,0 +1,1 @@
+provider "adguard" {}

dhoppeIT/terraform-cloudflare-account

examples/default/providers.tf
@@ -1,0 +1,1 @@
+provider "cloudflare" {}